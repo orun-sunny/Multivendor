816d8d2c4ba954ca3e3ffa9fa9982012f9a39a0d
orun-sunny <sunny.mukut@gmail.com>
2023-07-09 18:05:50 +0600

    project

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
