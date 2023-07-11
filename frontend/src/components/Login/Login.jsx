af6bfcf68352ebabded7b1ee4799b4c7b7ad50ea
orun-sunny <sunny.mukut@gmail.com>
2023-07-11 18:11:07 +0600

    35min.15sec

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Login = () => {
-  return <div className="min-h-screen bg">Loginnnnnnnnnnn</div>;
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 ">
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Login;
